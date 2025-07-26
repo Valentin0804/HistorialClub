9cb29b6a5d243649cbd5802b4d92684d504d39c4
Valentin Molina <valenmoli_18@hotmail.com>
2025-07-26 02:59:16 -0300

    ALLOWED_HOSTS

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -38,8 +38,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 # Application definition
 
